dcfaf4cf955d7c81f500b1010bc16208df83eeea
Vangogh500 <kmatsuda@umass.edu>
2016-03-08 09:08:56 -0500

    bug fixes with merging

--- app/components/profileexchangebooklist.js
+++ app/components/profileexchangebooklist.js
@@ -7,16 +7,11 @@ export default class Profileexchangebooklist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
+
       exchange:[]
     };
   }
-=======
-      exchange : []
-  };
-}
->>>>>>> 54baee217335ea0e6864372dc1e44d9abc603104
-
+  
   refresh() {
     getExchangebook(this.props.data, (book) => {
       this.setState({exchange:book});
