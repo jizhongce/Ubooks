af422344e45381f223b3226667d40f3b2a21cf5f
jizhongce <jizhongce123@gmail.com>
2016-03-28 16:48:00 -0400

    add load the page from client in the server.js and delete the html file

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,19 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+
+
+//import the function
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+//end
+
+
+
+app.use(express.static('../client/build'));
+
+
+app.listen(3000, function() {
+   console.log('Example app listening on port 3000!');
+});
